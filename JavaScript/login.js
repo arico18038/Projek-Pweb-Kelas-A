aa3da9c5a1172dac3fbd11dd4a5b8fef54d4b406
arico18038 <2300018038@webmail.uad.ac.id>
2024-06-04 14:52:37 +0700

    uodate login dan register

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -5,19 +5,19 @@ const loginBtn = document.querySelector("label.login");
 const signupBtn = document.querySelector("label.signup");
 const signupLink = document.querySelector(".signup-link a");
 
-signupBtn.onclick = () => {
-    loginForm.style.marginLeft = "-50%";
-    loginText.style.marginLeft = "-50%";
-    signupText.style.marginLeft = "-50%";
-};
+// signupBtn.onclick = () => {
+//     loginForm.style.marginLeft = "-50%";
+//     loginText.style.marginLeft = "-50%";
+//     signupText.style.marginLeft = "-50%";
+// };
 
-loginBtn.onclick = () => {
-    loginForm.style.marginLeft = "0%";
-    loginText.style.marginLeft = "0%";
-    signupText.style.marginLeft = "0%";
-};
+// loginBtn.onclick = () => {
+//     loginForm.style.marginLeft = "0%";
+//     loginText.style.marginLeft = "0%";
+//     signupText.style.marginLeft = "0%";
+// };
 
-signupLink.onclick = (e) => {
-    e.preventDefault();
-    signupBtn.click();
-};
+// signupLink.onclick = (e) => {
+//     e.preventDefault();
+//     signupBtn.click();
+// };
