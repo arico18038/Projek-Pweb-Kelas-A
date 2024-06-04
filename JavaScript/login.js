98f0e23a7a118ccaaa702ba830537944b353cd53
FadelJoan <2300018031@webmail.uad.ac.id>
2024-06-04 16:11:28 +0700

    tambah about

---
+++ JavaScript/login.js
@@ -0,0 +1,23 @@
+const loginText = document.querySelector(".title-text .login");
+const signupText = document.querySelector(".title-text .signup");
+const loginForm = document.querySelector(".form-container .form-inner");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector(".signup-link a");
+
+// signupBtn.onclick = () => {
+//     loginForm.style.marginLeft = "-50%";
+//     loginText.style.marginLeft = "-50%";
+//     signupText.style.marginLeft = "-50%";
+// };
+
+// loginBtn.onclick = () => {
+//     loginForm.style.marginLeft = "0%";
+//     loginText.style.marginLeft = "0%";
+//     signupText.style.marginLeft = "0%";
+// };
+
+// signupLink.onclick = (e) => {
+//     e.preventDefault();
+//     signupBtn.click();
+// };
