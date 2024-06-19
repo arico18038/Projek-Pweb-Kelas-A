eb1cf73fcfd0a4c9e3617a261f4ee477d42236af
SyamsuRizall <syamsurizal2220@gmail.com>
2024-06-19 13:52:37 +0700

    update login

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -1,23 +1,4 @@
-const loginText = document.querySelector(".title-text .login");
-const signupText = document.querySelector(".title-text .signup");
 const loginForm = document.querySelector(".form-container .form-inner");
-const loginBtn = document.querySelector("label.login");
-const signupBtn = document.querySelector("label.signup");
 const signupLink = document.querySelector(".signup-link a");
 
-// signupBtn.onclick = () => {
-//     loginForm.style.marginLeft = "-50%";
-//     loginText.style.marginLeft = "-50%";
-//     signupText.style.marginLeft = "-50%";
-// };
-
-// loginBtn.onclick = () => {
-//     loginForm.style.marginLeft = "0%";
-//     loginText.style.marginLeft = "0%";
-//     signupText.style.marginLeft = "0%";
-// };
-
-// signupLink.onclick = (e) => {
-//     e.preventDefault();
-//     signupBtn.click();
-// };
+// Tidak ada lagi event handler untuk slide, karena slide sudah dihapus
