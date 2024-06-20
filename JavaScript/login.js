793ad33ea2fd744e836557385491be217ba593a1
arico18038 <2300018038@webmail.uad.ac.id>
2024-06-20 14:25:02 +0700

    signup

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -1,4 +1,4 @@
 const loginForm = document.querySelector(".form-container .form-inner");
 const signupLink = document.querySelector(".signup-link a");
 
-// Tidak ada lagi event handler untuk slide, karena slide sudah dihapus
+
--- signup.php
+++ signup.php
@@ -9,18 +9,19 @@
 <body>
     <div class="wrapper">
         <div class="title">Sign Up</div>
-        <form>
+        <!-- Menambahkan action ke file PHP handler dan method POST -->
+        <form action="handle_signup.php" method="POST">
             <div class="field">
-                <input type="text" placeholder="Username" required>
+                <input type="text" name="username" placeholder="Username" required>
             </div>
             <div class="field">
-                <input type="email" placeholder="Email" required>
+                <input type="email" name="email" placeholder="Email" required>
             </div>
             <div class="field">
-                <input type="password" placeholder="Password" required>
+                <input type="password" name="password" placeholder="Password" required>
             </div>
             <div class="field">
-                <input type="password" placeholder="Confirm Password" required>
+                <input type="password" name="confirm_password" placeholder="Confirm Password" required>
             </div>
             <div class="btn">
                 <div class="btn-layer"></div>
